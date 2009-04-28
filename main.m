a22205309f3895cdd3823864544e4e60831402c1
Patrick Mosby <info@schreiblogade.de>
2009-04-28 12:26:33 +0200

    Initial commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  spgw4ffmpeg
+//
+//  Created by Patrick on 28.04.09.
+//  Copyright Patrick Mosby 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
