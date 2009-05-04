15fdf6756687b5a74cf200ab7b31f14ec9288203
Patrick Mosby <info@schreiblogade.de>
2009-05-04 20:20:47 +0200

    Define basic interface properties for AppController

--- AppController.h
+++ AppController.h
@@ -10,7 +10,20 @@
 
 
 @interface AppController : NSObject {
-
+    IBOutlet NSTextField    *inputFileField;
+    IBOutlet NSTextField    *outputFileField;
+    IBOutlet NSTextField    *startTimeCodeField;
+    IBOutlet NSTextField    *durationTimeCodeField;
+    
+    IBOutlet NSButton       *inputChooseButton;
+    IBOutlet NSButton       *outputChosseButton;
+    IBOutlet NSButton       *startTranscodeButton;
+    
+    IBOutlet NSMatrix       *qualityRadioGroup;
 }
+
+-(IBAction)startCancelTranscode:(id)sender;
+-(IBAction)showInputChooserPanel:(id)sender;
+-(IBAction)showOutputChooserPanel:(id)sender;
 
 @end
