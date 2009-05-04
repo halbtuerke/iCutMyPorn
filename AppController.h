ab92bc795eb8fdf4c19157aedc11019333d90d84
Patrick Mosby <info@schreiblogade.de>
2009-05-04 19:56:08 +0200

    Add AppController skeleton

---
+++ AppController.h
@@ -0,0 +1,16 @@
+//
+//  AppController.h
+//  spgw4ffmpeg
+//
+//  Created by Patrick on 04.05.09.
+//  Copyright 2009 Patrick Mosby. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AppController : NSObject {
+
+}
+
+@end
---
+++ AppController.m
@@ -0,0 +1,14 @@
+//
+//  AppController.m
+//  spgw4ffmpeg
+//
+//  Created by Patrick on 04.05.09.
+//  Copyright 2009 Patrick Mosby. All rights reserved.
+//
+
+#import "AppController.h"
+
+
+@implementation AppController
+
+@end
