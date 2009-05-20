c3f76a48c93ed1a98193b159ed9c0fe9db93f2ef
Patrick Mosby <info@schreiblogade.de>
2009-05-20 09:45:53 +0200

    Implement inputChooserPanel and its delegate

--- AppController.m
+++ AppController.m
@@ -21,7 +21,40 @@
 
 -(IBAction)showInputChooserPanel:(id)sender
 {
+    if ([inputChooseButton title] == @"Clear") {
+        [inputFileField setStringValue:@""];
+        [inputChooseButton setTitle:@"Choose"];
+        startButtonEnabled = startButtonEnabled - 2;
+        [self enableStartButton];
+    } else {
+        NSArray *fileTypes = [NSArray arrayWithObjects:@"flv", @"avi", @"mp4", @"mov", @"wmv", @"divx", @"h264", @"mkv", @"m4v", @"3gp", nil];
+        NSString *moviesDirectory = [NSHomeDirectory() stringByAppendingPathComponent:@"Movies"];
+        NSOpenPanel *panel = [NSOpenPanel openPanel];
 
+        // Run the open panel
+        [panel beginSheetForDirectory:moviesDirectory
+                                 file:nil
+                                types:fileTypes
+                       modalForWindow:mainWindow
+                        modalDelegate:self
+                       didEndSelector:@selector(inputChooserPanelDidEnd:returnCode:contextInfo:)
+                          contextInfo:NULL];
+
+    }
+}
+
+- (void)inputChooserPanelDidEnd:(NSOpenPanel *)openPanel
+             returnCode:(int)returnCode
+            contextInfo:(void *)x
+{
+    // Did they choose "Open"?
+    if (returnCode == NSOKButton) {
+        NSString *path = [openPanel filename];
+        [inputFileField setStringValue:path];
+        [inputChooseButton setTitle:@"Clear"];
+        [self enableStartButton];
+        // NSLog(@"inputFile: %@", path);
+    }
 }
 
 -(IBAction)showOutputChooserPanel:(id)sender
